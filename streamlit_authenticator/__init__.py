6db16f35de8c0cfa2235ac90335206098701d6b2
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-06 18:42:03 +0700

    Update __init__.py

--- streamlit_authenticator/__init__.py
+++ streamlit_authenticator/__init__.py
@@ -28,15 +28,15 @@ while  _RELEASE:
     except Exception as e:
         st.error(e)
         
-    if st.session_state["authentication_status"]:
-        authenticator.logout()
-        st.write(f'Welcome *{st.session_state["name"]}*')
-        st.title('Some content')
-    elif st.session_state["authentication_status"] is False:
+
+    if st.session_state["authentication_status"] is False:
         st.error('Username/password is incorrect')
     elif st.session_state["authentication_status"] is None:
         st.warning('Please enter your username and password')
-
+    elif st.session_state["authentication_status"]:
+        authenticator.logout()
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.title('Some content')
     # Creating a password reset widget
     if st.session_state["authentication_status"]:
         try:
@@ -58,14 +58,6 @@ while  _RELEASE:
     except Exception as e:
         st.error(e)
 
-    if st.session_state["authentication_status"]:
-        authenticator.logout()
-        st.write(f'Welcome *{st.session_state["name"]}*')
-        st.title('Some content')
-    elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect')
-    elif st.session_state["authentication_status"] is None:
-        st.warning('Please enter your username and password')
 
     # Creating a forgot password widget
     try:
