62c2be8ab4c2a36ce24257dc62bd7322540b6909
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-06 18:45:07 +0700

    Update __init__.py

--- streamlit_authenticator/__init__.py
+++ streamlit_authenticator/__init__.py
@@ -38,12 +38,13 @@ while  _RELEASE:
         st.write(f'Welcome *{st.session_state["name"]}*')
         st.title('Some content')
     # Creating a password reset widget
-    if st.session_state["authentication_status"]:
+    if st.session_state["authentication_status"] is True:
         try:
             if authenticator.reset_password(st.session_state["username"]):
                 st.success('Password modified successfully')
         except Exception as e:
             st.error(e)
+
 
     # Creating a new user registration widget
     try:
@@ -53,7 +54,7 @@ while  _RELEASE:
     except Exception as e:
         st.error(e)
 
-
+'''
     # Creating an update user details widget
     if st.session_state["authentication_status"]:
         try:
@@ -61,7 +62,7 @@ while  _RELEASE:
                 st.success('Entries updated successfully')
         except Exception as e:
             st.error(e)
-
+'''
     # Saving config file
     with open('config.yaml', 'w') as file:
         yaml.dump(config, file, default_flow_style=False)
